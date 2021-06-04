86643a1f1636e31948a4bf915e239cd17f6337e8
Jcrt <julio.rosatrindade@gmail.com>
2021-06-04 06:18:13 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { from, of } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    const grapes = ['Grape 1', 'Grape 2', 'Grape 3'];
+
+    of(...grapes).subscribe(console.log);
+
+    of(...grapes).subscribe(
+      grape => console.log(`Toma a ${grape}`),
+      err => console.log(`Deu ruim: ${err}`),
+      () => console.log('Cabô as grape')
+    );
+
+    from(grapes).subscribe(
+      item => console.log(`Resulting item .. ${item}`),
+      err => console.log(`Error occurred .. ${err}`),
+      () => console.log('Cabô')
+    );
+  }
   name = 'Angular';
 }
