e3664f3a79b291301fe233d1b73e5811a3ed301c
Jcrt <julio.rosatrindade@gmail.com>
2021-06-04 06:48:08 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { from, of } from 'rxjs';
+import { map, take, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'my-app',
@@ -9,20 +10,46 @@ import { from, of } from 'rxjs';
 export class AppComponent implements OnInit {
   ngOnInit(): void {
     const grapes = ['Grape 1', 'Grape 2', 'Grape 3'];
+    const nums = [1, 2, 3, 4];
 
+    console.log(">>> Example of 'of' simple");
     of(...grapes).subscribe(console.log);
 
+    console.log(">>> Example of 'of' with complete subscribe");
     of(...grapes).subscribe(
       grape => console.log(`Toma a ${grape}`),
       err => console.log(`Deu ruim: ${err}`),
       () => console.log('Cabô as grape')
     );
 
+    console.log(">>> Example of 'from' with complete subscribe");
     from(grapes).subscribe(
       item => console.log(`Resulting item .. ${item}`),
       err => console.log(`Error occurred .. ${err}`),
       () => console.log('Cabô')
     );
+
+    console.log(
+      ">>> Example of 'from' with complete subscribe and pipe functions"
+    );
+    of(...nums)
+      .pipe(
+        take(3),
+        map(item => {
+          const n = item - 2;
+          if (n === 0) {
+            throw new Error(`Erro quando subtrai 2 de ${item} e dá ${n}`);
+          }
+
+          return item;
+        }),
+        tap(item => console.log(`Debug it: ${item}`))
+      )
+      .subscribe(
+        num => console.log(`The number is ${num}`),
+        err => console.log(`Deu ruim: ${err}`),
+        () => console.log('Cabô os erros')
+      );
   }
   name = 'Angular';
 }
